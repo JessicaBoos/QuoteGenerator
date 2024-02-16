75b003c5be0e63053696ef966da627bc915c4329
JessicaBoos <jessica.boos28@gmail.com>
2024-02-16 14:59:32 +0100

    updated js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
-function generatePoem(event) {
+function generateQuote(event) {
   event.preventDefault();
   alert("Generating poem");
 
   new Typewriter("#quote", {
-    strings: ["Hello", "World"],
+    strings:
+      "Do the difficult things while they are easy and do the great things while they are small. A journey of a thousand miles must begin with a single step.",
     autoStart: true,
-    delay: 0.5,
+    delay: 10,
     cursor: "",
   });
 }
-new Typewriter("#quote", {
-  strings:
-    "Do the difficult things while they are easy and do the great things while they are small. A journey of a thousand miles must begin with a single step.",
-  autoStart: true,
-  delay: 10,
-  cursor: "",
-});
 
 let formElement = document.querySelector("#quotegenerator");
-formElement.addEventListener("submit", generatePoem);
+formElement.addEventListener("submit", generateQuote);
