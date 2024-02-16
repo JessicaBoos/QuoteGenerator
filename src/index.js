d69a7873e004664b54602b2dac1909173ca10deb
JessicaBoos <jessica.boos28@gmail.com>
2024-02-16 14:47:27 +0100

    added typewriter feature

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 function generatePoem(event) {
   event.preventDefault();
   alert("Generating poem");
+
+  new Typewriter("#quote", {
+    strings: ["Hello", "World"],
+    autoStart: true,
+    delay: 0.5,
+    cursor: "",
+  });
 }
+new Typewriter("#quote", {
+  strings:
+    "Do the difficult things while they are easy and do the great things while they are small. A journey of a thousand miles must begin with a single step.",
+  autoStart: true,
+  delay: 10,
+  cursor: "",
+});
 
 let formElement = document.querySelector("#quotegenerator");
 formElement.addEventListener("submit", generatePoem);
