d945dd01c3d562c0589c8f826bcd49a962cb4446
JessicaBoos <jessica.boos28@gmail.com>
2024-02-16 15:34:37 +0100

    Added AI integration

--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
-function generateQuote(event) {
-  event.preventDefault();
-  alert("Generating poem");
-
+function displayQuote(response) {
   new Typewriter("#quote", {
-    strings:
-      "Do the difficult things while they are easy and do the great things while they are small. A journey of a thousand miles must begin with a single step.",
+    strings: response.data.answer,
     autoStart: true,
     delay: 10,
     cursor: "",
   });
+  console.log("poem generated.");
 }
+
+function generateQuote(event) {
+  event.preventDefault();
+
+  let userInput = document.querySelector("#user-input");
+  let APIkey = "bb5982aa3c1a3d9fb3839bo024tffc09";
+  let prompt = userInput.value;
+  let context =
+    "Generate a motivational quote about the given prompt. Sign the quote with 'SheCodes AI' after the quote.";
+  let APIurl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${APIkey}`;
+
+  new Typewriter("#quote", {
+    strings: "Generating poem...",
+    autoStart: true,
+    delay: 10,
+    cursor: "",
+  });
+  console.log(`Generating poem...`);
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Conetxt: ${context}`);
+
+  axios.get(APIurl).then(displayQuote);
+}
+
+new Typewriter("#quote", {
+  strings:
+    "Do the difficult things while they are easy and do the great things while they are small. A journey of a thousand miles must begin with a single step.",
+  autoStart: true,
+  delay: 10,
+  cursor: "",
+});
 
 let formElement = document.querySelector("#quotegenerator");
 formElement.addEventListener("submit", generateQuote);
